322ed93ebd4e36415a71c7005bf9d45081060529
Kendrick Kihara <kendrick@mint>
2012-08-26 09:53:56 +0900

    started using _paths

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem "therubyracer", :require => 'v8'
 
 gem 'rails', '3.2.8'
+gem 'bootstrap-sass', '2.0.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', 
                                 :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
   end
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', 
                                 :text => "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -38,12 +38,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', 
                                 :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
@@ -53,12 +53,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', 
                                 :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
